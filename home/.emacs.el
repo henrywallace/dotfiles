fcc39674d0c929df8d5bd44af1519c85f162ebca
Henry Wallace <henry@henrywallace.io>
2017-11-18 16:03:24 -0500

    remove unused copy-word emacs def

--- home/.emacs.el
+++ home/.emacs.el
@@ -46,41 +46,6 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-(defun get-point (symbol &optional arg)
-  "get the point"
-  (funcall symbol arg)
-  (point)
-  )
-
-(defun copy-thing (begin-of-thing end-of-thing &optional arg)
-  "copy thing between beg & end into kill ring"
-  (save-excursion
-    (let ((beg (get-point begin-of-thing 1))
-	  (end (get-point end-of-thing arg)))
-      (copy-region-as-kill beg end)))
-  )
-
-(defun paste-to-mark(&optional arg)
-  "Paste things to mark, or to the prompt in shell-mode"
-  (let ((pasteMe
-     	 (lambda()
-     	   (if (string= "shell-mode" major-mode)
-	       (progn (comint-next-prompt 25535) (yank))
-	     (progn (goto-char (mark)) (yank) )))))
-    (if arg
-	(if (= arg 1)
-	    nil
-	  (funcall pasteMe))
-      (funcall pasteMe))
-    ))
-(defun copy-word (&optional arg)
-  "Copy words at point into kill-ring"
-  (interactive "P")
-  (copy-thing 'backward-word 'forward-word arg)
-  ;;(paste-to-mark arg)
-  )
-(global-set-key (kbd "C-c w")         (quote copy-word))
-
 ;; faster kbd for goto-line
 (global-set-key (kbd "C-f") 'goto-line)
 ;; (global-set-key (kbd "C-w") 'backward-kill-word)
