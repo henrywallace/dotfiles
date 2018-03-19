4d2b7df440bc0804f297cd71c5f8554dd34c3e4c
Henry Wallace <henry@henrywallace.io>
2018-03-19 00:20:45 -0400

    stow: add script to stow all 'packages' in dotfiles repo automatically

---
+++ stow.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+
+if ! hash stow &>/dev/null || ! hash fd &>/dev/null; then
+    echo 'please install `stow` and `fd` with install.sh'
+    exit 1
+fi
+
+PKGS=$(fd -t d)
+echo "stowing packages $(echo $PKGS)"
+stow $PKGS && echo 'success!' || echo 'failed to stow'
