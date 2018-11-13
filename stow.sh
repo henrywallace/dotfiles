bf325cb62dd2026d1f0e937464a07251ab85ba33
Henry Wallace <henry@henrywallace.io>
2018-11-13 00:38:09 +0000

    protect stow.sh against missing tree

--- stow.sh
+++ stow.sh
@@ -4,6 +4,8 @@ set -e
 
 PKGS=$(echo */)
 echo "stowing packages: $PKGS\n"
-tree -a -I *~ $PKGS
+if [[ -x "$(command -v tree)" ]]; then
+  tree -a -I *~ $PKGS
+fi
 
 stow $PKGS && echo 'success!' || echo 'failed to stow'
