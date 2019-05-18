bb91c676d2c8453f33799ff4ecd66a044d425255
Henry Wallace <h@henrywallace.io>
2019-05-18 12:01:52 -0400

    fix zsh update.sh, and slowly start moving away from stow

--- .zsh/update.sh
+++ .zsh/update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+D=$(dirname "$(readlink -f "$0")")
 for repo in zsh-autosuggestions zsh-syntax-highlighting; do
   tput setaf 4; echo "$repo"; tput sgr0
-  [ -d "$repo" ] || git clone "https://github.com/zsh-users/$repo"
-  git -C "$repo" pull
+  [ -d "$D/$repo" ] || git clone "https://github.com/zsh-users/$repo" "$D/$repo"
+  git -C "$D/$repo" pull
 done
--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+PKGS="$(echo */) .debug .zsh"
+echo "stowing packages: $PKGS"
+if [[ -x "$(command -v tree)" ]]; then
+  tree -a -I *~ $PKGS
+fi
+
+stow $PKGS && echo 'success!' || echo 'failed to stow'
+
+ln -sfn .zsh/update.sh ~/.zsh
 
